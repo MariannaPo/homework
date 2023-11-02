5d7c27ac25c5498b6c4fee7f3e9604bf866ec80d
Marianna_Po <mapodmasko@gmail.com>
2023-11-02 16:30:42 +0300

    correct1

--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {UserList} from './UserList';
 
 export const HW1 = () => {
@@ -34,7 +35,7 @@ export const HW1 = () => {
 
 	return (
 		<div id={'hw01'}>
-			{/*<UserList users={'НАЗВАНИЕ МАССИВА, КОТОРЫЙ МЫ ПЕРЕДАЕМ'} />*/}
+			<UserList users={users}/>
 		</div>
 	);
 };
